79d0a571154a04467d8a7a1eed545405a2e859c9
JERUKA9 <jeruka9@gmail.com>
2017-11-09 10:11:33 +0100

    Add files via upload

---
+++ Program01.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+
+{
+ int h,a;
+
+h=5;
+
+a=h;
+
+
+ printf("wprowadz haslo",a);
+
+ scanf("haslo",&h);
+
+ if (h==5)else (h<5) then ;
+ {
+
+    printf("zle haslo");
+
+    printf("haslo poprawne");
+
+ }
+
+ return 0;
+
+
+
