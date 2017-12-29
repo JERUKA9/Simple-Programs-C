edf3d7963beb1a801d6b1763066e512eaacfe713
JERUKA9 <jeruka9@gmail.com>
2017-12-29 16:16:32 +0100

    Add files via upload

---
+++ CALMA.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+int a,b,c,d,e,f,g;
+
+main()
+
+{
+
+
+puts("calculator");
+
+printf("write value",a);
+
+
+    scanf("%d" ,&a);
+
+
+    printf(" write value" ,b);
+
+    scanf("%d" ,&b);
+
+
+    c=a+b;
+
+    printf("total adition %d",c);
+
+d=a-b;
+
+printf("total substraction %d",d);
+
+e=a*b;
+
+printf("total multiplication %d",e);
+
+f=a/b;
+
+printf("total division %d",f);
+
+g=a%b;
+
+printf("total modulo %d ",g);
+
+
+
+
+
+
+
+
+    return 0;
+
+
+
+}
